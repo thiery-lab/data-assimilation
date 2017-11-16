5418e2775a547196cdffc1c1b6083ecfdfa34d93
Matt Graham <matthew.m.graham@gmail.com>
2017-11-16 17:33:29 +0800

    Adding documentation to Netto 79 model.

--- dapy/models/netto79.py
+++ dapy/models/netto79.py
@@ -1,6 +1,6 @@
 """One-dimensional model with non-linear dynamics and observation operator.
 
-Model originally defined in:
+Model originally proposed in:
 
 > M. L. A. Netto, L. Gimeno, and M. J. Mendes.
 > A new spline algorithm for non-linear filtering of discrete time systems.
@@ -8,15 +8,56 @@ Model originally defined in:
 """
 
 import numpy as np
-import scipy.linalg as la
 from dapy.models.base import AbstractModel, inherit_docstrings
 
 
+@inherit_docstrings
 class Netto79Model(AbstractModel):
+    """One-dimensional model with non-linear dynamics and observation operator.
+
+    State update defined as
+
+      z[t+1] = alpha * z[t] + beta * (z[t] / (1 + z[t]**2)) +
+               gamma  * cos(delta * t) + sigma_z * u[t]
+
+    with u[t] ~ N(0, 1) and z[0] ~ N(m, s**2).
+
+    Observed process defined by
+
+      x[t] = epsilon * z[t]**2 + sigma_x * v[t]
+
+    with v[t] ~ N(0, 1).
+
+    Standard parameter values assumed here are alpha = 0.5, beta = 25,
+    gamma = 8, delta = 1.2, epsilon = 0.05, m=10, s=5, sigma_z = 1,
+    sigma_x = 10**0.5 and T = 100 simulated time steps.
+
+    References:
+
+        M. L. A. Netto, L. Gimeno, and M. J. Mendes.
+        A new spline algorithm for non-linear filtering of discrete time
+        systems. Proceedings of the 7th Triennial World Congress, 1979.
+    """
 
     def __init__(self, rng, init_state_mean=10., init_state_std=5.,
                  state_noise_std=1., obser_noise_std=10.**0.5, alpha=0.5,
                  beta=25., gamma=8., delta=1.2, epsilon=0.05):
+        """
+        Args:
+            rng (RandomState): Numpy RandomState random number generator.
+            init_state_mean (float): Initial state distribution mean.
+            init_state_std (float): Initial state distribution standard
+                deviation.
+            state_noise_std (float): Standard deviation of additive Gaussian
+                noise in state update.
+            obser_noise_std (float): Standard deviation of additive Gaussian
+                noise in observations.
+            alpha (float): Coefficient in non-linear state update.
+            beta (float): Coefficient in non-linear state update.
+            gamma (float): Coefficient in non-linear state update.
+            delta (float): Coefficient in non-linear state update.
+            epsilon (float): Coefficient in non-linear observation function.
+        """
         self.init_state_mean = init_state_mean
         self.init_state_std = init_state_std
         self.state_noise_std = state_noise_std
@@ -26,7 +67,7 @@ class Netto79Model(AbstractModel):
         self.gamma = gamma
         self.delta = delta
         self.epsilon = epsilon
-        super(Netto79Model, self).__init__(1, 1, rng)
+        super(Netto79Model, self).__init__(dim_z=1, dim_x=1, rng=rng)
 
     def init_state_sampler(self, n=None):
         if n is None:
