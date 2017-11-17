a7bbfced8076f73d11a3147592c1f558715dc1c6
Matt Graham <matthew.m.graham@gmail.com>
2017-11-17 14:32:00 +0800

    Adding package level imports of model classes.

--- dapy/models/__init__.py
+++ dapy/models/__init__.py
@@ -1,5 +1,10 @@
 """Python package for data assimilation example models."""
 
+from dapy.models.linear import LinearGaussianModel
+from dapy.models.netto79 import Netto79Model
+from dapy.models.lorenz63 import Lorenz63Model
+
+
 __all__ = ['linear', 'netto79', 'lorenz63', 'lorenz96']
 __authors__ = 'Matt Graham'
 __license__ = 'MIT'
