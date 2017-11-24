528ea481e1e620c39e881c1c875efba3398daaf5
Matt Graham <matthew.m.graham@gmail.com>
2017-11-24 16:55:36 +0800

    Making Netto79Model derive from DiagonalGaussianModel.

--- dapy/models/netto79.py
+++ dapy/models/netto79.py
@@ -8,11 +8,11 @@ Model originally proposed in:
 """
 
 import numpy as np
-from dapy.models.base import AbstractModel, inherit_docstrings
+from dapy.models.base import DiagonalGaussianModel, inherit_docstrings
 
 
 @inherit_docstrings
-class Netto79Model(AbstractModel):
+class Netto79Model(DiagonalGaussianModel):
     """One-dimensional model with non-linear dynamics and observation operator.
 
     State update defined as
@@ -58,28 +58,15 @@ class Netto79Model(AbstractModel):
             delta (float): Coefficient in non-linear state update.
             epsilon (float): Coefficient in non-linear observation function.
         """
-        self.init_state_mean = init_state_mean
-        self.init_state_std = init_state_std
-        self.state_noise_std = state_noise_std
-        self.obser_noise_std = obser_noise_std
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
         self.delta = delta
         self.epsilon = epsilon
-        super(Netto79Model, self).__init__(dim_z=1, dim_x=1, rng=rng)
-
-    def init_state_sampler(self, n=None):
-        if n is None:
-            return (
-                self.init_state_mean +
-                self.rng.normal(size=(1,)) * self.init_state_std
-            )
-        else:
-            return (
-                self.init_state_mean +
-                self.rng.normal(size=(n, 1)) * self.init_state_std
-            )
+        super(Netto79Model, self).__init__(
+            dim_z=1, dim_x=1, rng=rng, init_state_mean=init_state_mean,
+            init_state_std=init_state_std, state_noise_std=state_noise_std,
+            obser_noise_std=obser_noise_std)
 
     def next_state_func(self, z, t):
         return (
@@ -87,37 +74,5 @@ class Netto79Model(AbstractModel):
             self.gamma * np.cos(self.delta * t)
         )
 
-    def next_state_sampler(self, z, t):
-        return (
-            self.next_state_func(z, t) +
-            self.state_noise_std * self.rng.normal(size=z.shape)
-        )
-
-    def observation_func(self, z):
+    def observation_func(self, z, t):
         return self.epsilon * z**2
-
-    def observation_sampler(self, z, t):
-        return (
-            self.observation_func(z) +
-            self.rng.normal(size=z.shape) * self.obser_noise_std
-        )
-
-    def log_prob_dens_init_state(self, z):
-        return -(
-            0.5 * ((z - self.init_state_mean) / self.init_state_std)**2 +
-            0.5 * np.log(2 * np.pi) + np.log(self.init_state_std)
-        ).sum(-1)
-
-    def log_prob_dens_state_trans(self, z_n, z_c, t):
-        return -(
-            0.5 * ((z_n - self.next_state_func(z_c, t)) /
-                   self.state_noise_std)**2 +
-            0.5 * np.log(2 * np.pi) + np.log(self.state_noise_std)
-        ).sum(-1)
-
-    def log_prob_dens_obs_gvn_state(self, x, z, t):
-        return -(
-            0.5 * ((x - self.observation_func(z)) /
-                   self.obser_noise_std)**2 +
-            0.5 * np.log(2 * np.pi) + np.log(self.obser_noise_std)
-        ).sum(-1)
