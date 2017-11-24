523dee05feb59c6970788a995e55e3e259869984
Matt Graham <matthew.m.graham@gmail.com>
2017-11-24 16:34:32 +0800

    Adding package imports for fluid simulation module.

--- dapy/models/__init__.py
+++ dapy/models/__init__.py
@@ -4,8 +4,9 @@ from dapy.models.linear import LinearGaussianModel
 from dapy.models.netto79 import Netto79Model
 from dapy.models.lorenz63 import Lorenz63Model
 from dapy.models.lorenz96 import Lorenz96Model
+from dapy.models.fluidsim2d import FluidSim2DModel
 
 
-__all__ = ['linear', 'netto79', 'lorenz63', 'lorenz96']
+__all__ = ['linear', 'netto79', 'lorenz63', 'lorenz96', 'fluidsim2d']
 __authors__ = 'Matt Graham'
 __license__ = 'MIT'
