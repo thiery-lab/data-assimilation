4de84b463c8afe03639ff3f82909948a69ae4469
Matt Graham <matthew.m.graham@gmail.com>
2018-02-15 16:48:00 +0800

    Generalising localisation functions.

--- dapy/inference/localisation.py
+++ dapy/inference/localisation.py
@@ -62,47 +62,50 @@ class LocalisationFunction(object):
     Assumes a Euclidean distance function between points.
     """
 
-    def __init__(self, state_coords, obser_coords, localisation_radius,
+    def __init__(self, coords_a, coords_b, localisation_radius,
                  weighting_function=gaspari_and_cohn_weighting,
                  use_cache=True):
         """
         Args:
-            state_coords (array): Two-dimensional array of shape
-                `(n_state_points, n_dim)` where `n_state_points` is the number
-                of spatial points over which the system state is defined and
-                `n_dim` is the spatial dimensionality. Each row corresponds to
-                a set of spatial coordinates.
-            obser_coords (array): Two-dimensional array of shape
-                `(n_obser_points, n_dim)` where `n_obser_points` is the number
-                of spatial points at which the system is observed and  `n_dim`
-                is the spatial dimensionality. Each row corresponds to
-                a set of spatial coordinates.
+            coords_a (array): Two-dimensional array of shape
+                `(n_coords_a, n_dim)` where `n_coords_a` is the fixed number
+                of spatial points at which the distances and corresponding
+                localisation weights to a second set of spatial points will
+                need to be computed and `n_dim` is the spatial dimensionality.
+                Each row corresponds to a set of spatial coordinates.
+            coords_b (array): Two-dimensional array of shape
+                `(n_coords_b, n_dim)` where `n_coords_b` is the number
+                of spatial points at which the distances to a point from
+                the `coords_a` array will be computed  and `n_dim` is the
+                spatial dimensionality. Each row corresponds to a set of
+                spatial coordinates.
             localisation_radius (float): Positive scale parameter used to
-                define distance over which observations are considered 'local'
-                to a state spatial point.
+                define distance over which points are considered 'local'
+                to another point in space.
             weighting_function (function): Function which given an array of
                 distances and a scale parameter returns an array of weights
                 between 0 and 1 with 1 corresponding to a zero distance.
-            use_cache (boolean): Whether to cache the calculate observation
-                indices and weights for each index to avoid recalculation on
-                future calls. For large grids this may become memory-heavy.
+            use_cache (boolean): Whether to cache the calculated indices and
+                weights for each index to avoid recalculation on future calls.
+                For large grids this may become memory-heavy.
         """
-        self.state_coords = state_coords
-        self.obser_coords = obser_coords
+        self.coords_a = coords_a
+        self.coords_b = coords_b
+        self.n_coords_a = coords_a.shape[0]
+        self.n_coords_b = coords_b.shape[0]
         self.localisation_radius = localisation_radius
         self.weighting_function = weighting_function
         self.use_cache = use_cache
         if use_cache:
-            self._cache = [None] * state_coords.shape[0]
+            self._cache = [None] * self.n_coords_a
 
-    def observation_distances(self, point):
-        return ((point - self.obser_coords)**2).sum(-1)**0.5
+    def observation_distances(self, point_a):
+        return ((point_a - self.coords_b)**2).sum(-1)**0.5
 
     def __call__(self, index):
         if self.use_cache and self._cache[index] is not None:
             return self._cache[index]
-        state_point = self.state_coords[index]
-        dists = self.observation_distances(state_point)
+        dists = self.observation_distances(self.coords_a[index])
         weights = self.weighting_function(dists, self.localisation_radius)
         nz_weights = weights > 0.
         indices = np.nonzero(nz_weights)[0]
@@ -118,25 +121,26 @@ class PeriodicLocalisationFunction(LocalisationFunction):
     Assumes a (wrapped) Euclidean distance function between points.
     """
 
-    def __init__(self, state_coords, state_extents, obser_coords,
-                 localisation_radius,
+    def __init__(self, coords_a, coords_b, extents, localisation_radius,
                  weighting_function=gaspari_and_cohn_weighting,
                  use_cache=True):
         """
         Args:
-            state_coords (array): Two-dimensional array of shape
-                `(n_state_points, n_dim)` where `n_state_points` is the number
-                of spatial points over which the system state is defined and
-                `n_dim` is the spatial dimensionality. Each row corresponds to
-                a set of spatial coordinates.
-            state_extents (tuple): Tuple giving extents of cuboidal spatial
-                domain state is defined over with domain assumed to wrap at
+            coords_a (array): Two-dimensional array of shape
+                `(n_coords_a, n_dim)` where `n_coords_a` is the fixed number
+                of spatial points at which the distances and corresponding
+                localisation weights to a second set of spatial points will
+                need to be computed and `n_dim` is the spatial dimensionality.
+                Each row corresponds to a set of spatial coordinates.
+            coords_b (array): Two-dimensional array of shape
+                `(n_coords_b, n_dim)` where `n_coords_b` is the number
+                of spatial points at which the distances to a point from
+                the `coords_a` array will be computed  and `n_dim` is the
+                spatial dimensionality. Each row corresponds to a set of
+                spatial coordinates.
+            extents (tuple): Tuple giving extents of cuboidal spatial domain
+                state is defined over with domain assumed to wrap at
                 boundaries.
-            obser_coords (array): Two-dimensional array of shape
-                `(n_obser_points, n_dim)` where `n_obser_points` is the number
-                of spatial points at which the system is observed and  `n_dim`
-                is the spatial dimensionality. Each row corresponds to
-                a set of spatial coordinates.
             localisation_radius (float): Positive scale parameter used to
                 define distance over which observations are considered 'local'
                 to a state spatial point.
@@ -148,15 +152,14 @@ class PeriodicLocalisationFunction(LocalisationFunction):
                 on future calls. For large grids this may become memory-heavy.
         """
         super(PeriodicLocalisationFunction, self).__init__(
-            state_coords=state_coords,
-            obser_coords=obser_coords,
+            coords_a=coords_a, coords_b=coords_b,
             localisation_radius=localisation_radius,
             weighting_function=weighting_function,
             use_cache=use_cache
         )
-        self.state_extents = state_extents
+        self.extents = extents
 
-    def observation_distances(self, point):
-        deltas = np.abs(point - self.obser_coords)
+    def observation_distances(self, point_a):
+        deltas = np.abs(point_a - self.coords_b)
         return (np.minimum(deltas,
-                           self.state_extents - deltas)**2).sum(-1)**0.5
+                           self.extents - deltas)**2).sum(-1)**0.5
