3948b3bb727b4b9039bb121c823cf0920a676471
Matt Graham <matthew.m.graham@gmail.com>
2018-02-20 09:15:07 +0800

    Adding ot module to package __init__.py.

--- dapy/__init__.py
+++ dapy/__init__.py
@@ -1,5 +1,5 @@
 """Python package for data assimilation methods."""
 
-__all__ = ['inference', 'integrators', 'models']
+__all__ = ['inference', 'integrators', 'models', 'ot']
 __authors__ = 'Matt Graham'
 __license__ = 'MIT'
