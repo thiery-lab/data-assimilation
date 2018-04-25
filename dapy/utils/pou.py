71620c51fe1b8105aee4af93dec24cee1d48a512
Matt Graham <matthew.m.graham@gmail.com>
2018-04-25 18:40:00 +0800

    Clearning squared-cosine PoU implementations

--- dapy/utils/pou.py
+++ dapy/utils/pou.py
@@ -133,6 +133,7 @@ class GaspariAndCohn1dPartitionOfUnityBasis(object):
 
 
 class SquaredCosine1dPartitionOfUnityBasis(object):
+    """PoU on 1D grid using squared cosine compact smooth basis functions."""
 
     def __init__(self, n_grid, n_bases):
         self.n_grid = n_grid
@@ -142,31 +143,27 @@ class SquaredCosine1dPartitionOfUnityBasis(object):
              np.pi * self.n_bases / 2.)
         self.s = np.sin(u)**2
         self.c = np.cos(u)**2
-        self.norm = self.patch_half_dim
+
+    def split_into_patches_and_scale(self, f):
+        f_1d = np.reshape(f, (-1, self.n_grid))
+        f_1d_s = f_1d * self.s
+        f_1d_c = np.roll(f_1d * self.c, shift=self.patch_half_dim, axis=1)
+        f_1d_scaled_stack = np.stack([f_1d_s, f_1d_c], axis=1)
+        return f_1d_scaled_stack.reshape(
+            f.shape[:-1] + (self.n_bases,  2 * self.patch_half_dim))
 
     def integrate_against_bases(self, f):
         f_1d = np.reshape(f, (-1, self.n_grid))
         f_1d_s = f_1d * self.s
         f_1d_c = np.roll(f_1d * self.c, shift=self.patch_half_dim, axis=1)
         f_1d_scaled_stack = np.stack([f_1d_s, f_1d_c], axis=1)
-        return f_1d_scaled_stack.reshape(
-            (-1, 2, self.n_bases // 2,  2 * self.patch_half_dim)
-        ).sum(-1).reshape(f.shape[:-1] + (self.n_bases,)) / self.norm
-
-    def split_into_patches_and_scale(self, f):
-        n_particle, dim_field, n_grid = f.shape
-        f_1d = f.reshape((-1, self.n_grid))
-        f_1d_s = f_1d * self.s
-        f_1d_c = np.roll(f_1d * self.c, shift=self.patch_half_dim, axis=1)
-        f_1d_scaled_stack = np.stack([f_1d_s, f_1d_c], axis=1)
-        return f_1d_scaled_stack.reshape(
-            (n_particle, dim_field, self.n_bases, 2 * self.patch_half_dim))
+        return self.split_into_patches_and_scale(f).sum(-1)
 
     def combine_patches(self, f_patches):
         n_particle, dim_field = f_patches.shape[:2]
         f_1d_stacked = f_patches.reshape(
             (n_particle * dim_field, 2, self.n_grid))
-        f_1d_s = f_1d_stacked[:, 0, ]
+        f_1d_s = f_1d_stacked[:, 0, :]
         f_1d_c = np.roll(
             f_1d_stacked[:, 1, :], shift=-self.patch_half_dim, axis=1)
         return (f_1d_s + f_1d_c).reshape((n_particle, dim_field, self.n_grid))
@@ -192,7 +189,6 @@ class SquaredCosine2dPartitionOfUnityBasis(object):
         self.s1 = np.sin(u1)**2
         self.c0 = np.cos(u0)**2
         self.c1 = np.cos(u1)**2
-        self.norm = self.patch_half_dim[0] * self.patch_half_dim[1]
 
     def integrate_against_bases(self, f):
         f_2d = np.reshape(f, (-1,) + self.grid_shape)
@@ -208,7 +204,7 @@ class SquaredCosine2dPartitionOfUnityBasis(object):
         return f_2d_scaled_stack.reshape(
             (-1, 4, self.bases_grid_shape[0] // 2,  2 * self.patch_half_dim[0],
              self.bases_grid_shape[1] // 2, 2 * self.patch_half_dim[1])
-        ).sum((3, 5)).reshape(f.shape[:-1] + (self.n_bases,)) / self.norm
+        ).sum((3, 5)).reshape(f.shape[:-1] + (self.n_bases,))
 
     def split_into_patches_and_scale(self, f):
         n_particle, dim_field, n_grid = f.shape
