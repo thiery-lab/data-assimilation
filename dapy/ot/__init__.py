7fe10c2e230f120ffc3fdd73ead1df29a2fffd46
Matt Graham <matthew.m.graham@gmail.com>
2018-04-26 09:12:39 +0800

    Changing OT module to use new Cython solvers

--- dapy/ot/__init__.py
+++ dapy/ot/__init__.py
@@ -1,95 +1,125 @@
 """Optimal transport solver functions."""
 
 import warnings
-import ot
 import numpy as np
-from dapy.ot.batch_solvers import sequential_ot_solve, parallel_ot_solve
+from scipy.special import logsumexp
+from dapy.ot.solvers import (
+    solve_optimal_transport_network_simplex,
+    solve_optimal_transport_network_simplex_batch)
+try:
+    import ot
+    POT_AVAILABLE = True
+except ImportError:
+    POT_AVAILABLE = False
 
 DEFAULT_MAX_ITER = 100000
+DEFAULT_SUM_DIFF_TOLERANCE = 1e-8
 
 
-def log_sum_exp(x):
-    """Compute logarithm of sum of exponents with improved numerical stability.
-
-    Args:
-        x (array): One-dimension arrray of values to calculate log-sum-exp of.
-
-    Returns:
-        Scalar corresponding to logarith of sum of exponents of x values.
-    """
-    x_max = x.max()
-    return x_max + np.log(np.exp(x - x_max).sum())
+def solve_optimal_transport_exact(
+        source_dist, target_dist, cost_matrix, max_iter=DEFAULT_MAX_ITER,
+        sum_diff_tolerance=DEFAULT_SUM_DIFF_TOLERANCE):
+    trans_matrix, cost, results_code = (
+        solve_optimal_transport_network_simplex(
+            source_dist, target_dist, cost_matrix, max_iter=max_iter,
+            sum_diff_tolerance=sum_diff_tolerance))
+    if not results_code == 1:
+        warnings.warn('OT solution did not converge. Error codes: {9}'
+                      .format(results_code))
+    return trans_matrix
 
 
 def solve_optimal_transport_exact_batch(
-        cost_matrices, target_marginals, max_iter=DEFAULT_MAX_ITER,
-        num_threads=1):
-    n_particle, n_bases = target_marginals.shape
-    target_marginals /= target_marginals.sum(0)[None, :]
-    mu = target_marginals.T
-    nu = np.ones(mu.shape) / n_particle
-    cost_matrices = np.rollaxis(cost_matrices, 2, 0)
-    if num_threads == 1:
-        trans_matrices, costs, results_codes = sequential_ot_solve(
-            np.ascontiguousarray(mu), nu, np.ascontiguousarray(cost_matrices))
-    else:
-        trans_matrices, costs, results_codes = parallel_ot_solve(
-            np.ascontiguousarray(mu), nu, np.ascontiguousarray(cost_matrices),
-            max_iter=max_iter, num_threads=num_threads)
+        source_dists, target_dists, cost_matrices, max_iter=DEFAULT_MAX_ITER,
+        sum_diff_tolerance=DEFAULT_SUM_DIFF_TOLERANCE, n_thread=1):
+    trans_matrices, costs, results_codes = (
+        solve_optimal_transport_network_simplex_batch(
+            source_dists, target_dists, cost_matrices, max_iter=max_iter,
+            sum_diff_tolerance=sum_diff_tolerance, n_thread=n_thread))
     if not np.all(results_codes == 1):
-        warnings.warn('{0}/{1} of OT solutions did not converge'
-                     .format(np.sum(results_codes != 1), n_bases))
-    return np.ascontiguousarray(trans_matrices.T * n_particle)
+        err_codes = np.unique(results_codes[results_codes != 1])
+        warnings.warn('{0}/{1} of OT solutions did not converge. '
+                      'Error codes: {2}'
+                      .format(np.sum(results_codes != 1),
+                              source_dists.shape[0], err_codes))
+    return trans_matrices
 
 
 def solve_optimal_transport_sinkhorn_batch(
-        cost_matrices, target_marginals, epsilon, n_iter):
-    n_particle, n_bases = target_marginals.shape
+        source_dists, target_dists, cost_matrices, epsilon, n_iter):
 
     def modified_cost(u, v):
-        return (-cost_matrices + u[:, None, :] + v[None, :, :]) / epsilon
+        return (-cost_matrices + u[:, None, :] + v[:, :, None]) / epsilon
 
-    log_mu = -np.ones((n_particle, n_bases)) * np.log(n_particle)
-    log_nu = (
-        np.log(target_marginals) - np.log(target_marginals.sum(0))[None])
+    log_mu = np.log(source_dists)
+    log_nu = np.log(target_dists)
     u = 0 * log_mu
     v = 0 * log_nu
     for i in range(n_iter):
-        u = epsilon * (log_mu - log_sum_exp(modified_cost(u, v), 1)) + u
-        v = epsilon * (log_nu - log_sum_exp(modified_cost(u, v), 0)) + v
-        pi = np.exp(modified_cost(u, v))
-    max_marginal_error = np.max(np.abs(pi.sum(1) - 1. / n_particle))
+        u = epsilon * (log_mu - logsumexp(modified_cost(u, v), 2)) + u
+        v = epsilon * (log_nu - logsumexp(modified_cost(u, v), 1)) + v
+    trans_matrices = np.exp(modified_cost(u, v))
+    max_marginal_error = np.max(np.abs(trans_matrices.sum(1) - source_dists))
     if max_marginal_error > 1e-8:
         warnings.warn('Poor Sinkhorn--Knopp convergence. '
                       'Max absolute marginal difference: ({0:.2e})'
                       .format(max_marginal_error))
-    return pi * n_particle
+    return trans_matrices
 
 
-def solve_optimal_transport_exact_pot(
-        cost_matrices, target_marginals, max_iter=DEFAULT_MAX_ITER):
-    n_particle, n_bases = target_marginals.shape
-    u = ot.unif(n_particle)
-    trans_matrices = np.empty((n_particle, n_particle, n_bases))
-    target_marginals /= target_marginals.sum(0)[None, :]
-    for r in range(n_bases):
-        trans_matrices[:, :, r] = ot.emd(
-            np.ascontiguousarray(target_marginals[:, r]), u,
-            np.ascontiguousarray(cost_matrices[:, :, r]),
-            numItermax=max_iter).T
-    return trans_matrices * n_particle
+if POT_AVAILABLE:
 
+    def solve_optimal_transport_exact_pot(
+            source_dist, target_dist, cost_matrix, max_iter=DEFAULT_MAX_ITER):
+        trans_matrix, log = ot.emd(
+            np.ascontiguousarray(source_dist),
+            np.ascontiguousarray(target_dist),
+            np.ascontiguousarray(cost_matrix), numItermax=max_iter, log=True)
+        if not log['result_code'] == 1:
+            warnings.warn('OT solution did not converge. Error codes: {0}'
+                          .format(log['result_code']))
+        return trans_matrix
 
-def solve_optimal_transport_sinkhorn_pot(
-        cost_matrices, target_marginals, epsilon, max_iter=DEFAULT_MAX_ITER):
-    n_particle, n_bases = target_marginals.shape
-    u = ot.unif(n_particle)
-    trans_matrices = np.empty((n_particle, n_particle, n_bases))
-    target_marginals /= target_marginals.sum(0)[None, :]
-    for r in range(n_bases):
-        trans_matrices[:, :, r] = ot.sinkhorn(
-            np.ascontiguousarray(target_marginals[:, r]), u,
-            np.ascontiguousarray(cost_matrices[:, :, r]),
-            epsilon, 'sinkhorn_stabilized',
-            numItermax=max_iter).T
-    return trans_matrices * n_particle
+    def solve_optimal_transport_sinkhorn_pot(
+            source_dist, target_dist, cost_matrix, epsilon,
+            max_iter=DEFAULT_MAX_ITER):
+        trans_matrix = ot.sinkhorn(
+                np.ascontiguousarray(source_dists[p]),
+                np.ascontiguousarray(target_dists[p]),
+                np.ascontiguousarray(cost_matrices[p]), reg=epsilon,
+                method='sinkhorn_stabilized', numItermax=max_iter)
+        return trans_matrix
+
+    def solve_optimal_transport_exact_batch_pot(
+            source_dists, target_dists, cost_matrices,
+            max_iter=DEFAULT_MAX_ITER):
+        n_problem, n_particle = source_dists.shape
+        trans_matrices = np.empty((n_problem, n_particle, n_particle))
+        result_codes = np.empty((n_problem,))
+        for p in range(n_problem):
+            trans_matrices[p], log = ot.emd(
+                np.ascontiguousarray(source_dists[p]),
+                np.ascontiguousarray(target_dists[p]),
+                np.ascontiguousarray(cost_matrices[p]), numItermax=max_iter,
+                log=True)
+            result_codes[p] = log['result_code']
+        if not np.all(results_codes == 1):
+            err_codes = np.unique(results_codes[results_codes != 1])
+            warnings.warn('{0}/{1} of OT solutions did not converge. '
+                          'Error codes: {2}'
+                          .format(np.sum(results_codes != 1),
+                                  source_dists.shape[0], err_codes))
+        return trans_matrices
+
+    def solve_optimal_transport_sinkhorn_batch_pot(
+            source_dists, target_dists, cost_matrices, epsilon,
+            max_iter=DEFAULT_MAX_ITER):
+        n_problem, n_particle = source_dists.shape
+        trans_matrices = np.empty((n_problem, n_particle, n_particle))
+        for p in range(n_problem):
+            trans_matrices[p] = ot.sinkhorn(
+                np.ascontiguousarray(source_dists[p]),
+                np.ascontiguousarray(target_dists[p]),
+                np.ascontiguousarray(cost_matrices[p]), reg=epsilon,
+                method='sinkhorn_stabilized', numItermax=max_iter)
+        return trans_matrices
