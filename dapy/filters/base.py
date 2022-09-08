Refactor assimilation update and correct optimal proposal weight computation

dapy/filters/base.py
@@ -1,8 +1,9 @@
 """Abstract base classes for ensemble filters for inference in state space models."""
 
 import abc
-from typing import Sequence, Dict
+from typing import Sequence, Dict, Tuple
 import numpy as np
+from numpy.typing import ArrayLike
 from numpy.random import Generator
 from dapy.models.base import AbstractModel
 from dapy.utils.progressbar import ProgressBar
@@ -24,36 +25,38 @@ class AbstractEnsembleFilter(abc.ABC):
         self,
         model: AbstractModel,
         rng: Generator,
-        state_particles: np.ndarray,
-        observation: np.ndarray,
+        previous_states: ArrayLike,
+        predicted_states: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> np.ndarray:
+    ) -> Tuple[ArrayLike, Dict[str, ArrayLike]]:
         """Adjust state particle ensemble for observations at current time index.
 
-        The assimilation update transforms the prior predictive empirical distribution
-        represented by the state particle ensemble to a particle ensemble corresponding
-        to an empricial estimate of the posterior (filtering) distribution given the
-        observations at the current time index.
+        The assimilation update transforms a particle ensemble `predicted_states`
+        corresponding to an empirical estimate of the predictive distribution at time
+        index `time_index` for state space model `model` to an empirical estimate of
+        the filtering distribution at the same time index given the observation vector
+        at this time index `observation`.
 
         Args:
             model: State-space model to perform assimilation update with.
-            rng: NumPy random number geneator.
-            state_particles: Two-dimensional array of shape
-                `(num_particle, dim_state)` with each row a state particle generated
-                by simulating model dynamics forward from analysis ensemble at
-                previous time step.
-            observation: Observations at current time index `time_index`.
-            time_index: Current time index to assimilate the observations for.
+            rng: NumPy random number generator to use for sampling any random variates.
+            previous_states: Two-dimensional array of shape `(num_particle, dim_state)`
+                with each row a state particle in ensemble representing empirical
+                estimate of filtering distribution at time index `time_index - 1`.
+            predicted_states: Two-dimensional array of shape
+                `(num_particle, dim_state)` with each row a state particle in ensemble
+                representing empirical estimate of predictive distribution at time index
+                `time_index`.
+            observation: Observations at time index `time_index`.
+            time_index: Time index to assimilate the observations for.
 
         Returns:
-            post_state_particles (array): Two-dimensional array of shape
-                `(n_particle, dim_z)` with each row a state particle in
-                analysis ensemble.
-            post_state_mean (array): One-dimensional array of shape `(dim_z, )`
-                corresponding to estimated mean of state analysis distribution.
-            post_state_std (array): One-dimensional array of shape `(dim_z, )`
-                corresponding to estimated per-dimension standard deviations
-                of analysis distribution.
+            updated_states: Two-dimensional array of shape `(num_particle, dim_state)`
+                with each row a state particle in ensemble representing empirical
+                estimate of filtering distribution at time index `time_index`.
+            statistics: Dictionary of ensemble statistics with string keys and array
+                values.
         """
 
     def _perform_model_specific_initialization(
@@ -64,12 +67,12 @@ class AbstractEnsembleFilter(abc.ABC):
     def filter(
         self,
         model: AbstractModel,
-        observation_sequence: np.ndarray,
+        observation_sequence: ArrayLike,
         observation_time_indices: Sequence[int],
         num_particle: int,
         rng: Generator,
         return_particles: bool = False,
-    ) -> Dict[str, np.ndarray]:
+    ) -> Dict[str, ArrayLike]:
         """Compute particle ensemble approximations of filtering distributions.
 
         Args:
@@ -81,27 +84,26 @@ class AbstractEnsembleFilter(abc.ABC):
             observation_time_indices: Sequence of time (step) indices at which state is
                 observed. The sequence elements must be integers. The length of the
                 sequence must correspond to the number of observation times
-                `num_observation_time` represented in the `observation_sequences_array`.
+                `num_observation_time` represented in the `observation_sequence`.
             num_particle: Number of particles to use in ensembles used to approximate
                 filtering distributions at each time index.
-            rng: NumPy random number generator object.
+            rng: NumPy random number generator object to use to sample random variates.
             return_particles: Whether to return two-dimensional array of shape
                 `(num_observation_time, num_particle, dim_state)` containing all state
                 particles at each observation time index. Potentially memory-heavy for
                 for models with large state dimensions.
         Returns:
-            Dictionary containing arrays of filtering distribution parameters -
+            Dictionary containing arrays of filtering distribution statistics -
                 state_mean_sequence: Array of filtering distribution means at all
                     observation time indices. Shape `(num_observation_time, dim_state)`.
                 state_std_sequence: Array of filtering distribution standard deviations
                     at all at all observation time indices. Shape
                     `(num_observation_time, dim_state)`.
-                state_particles_sequence: Array of state particle ensemble
-                    approximations to filtering distributions at at all observation time
-                    indices. Only returned if `return_particles == True`. Shape
+                state_particles_sequence: Array of state particles representing
+                    empirical estimates of filtering distributions at at all observation
+                     time indices. Only returned if `return_particles == True`. Shape
                     `(num_observation_time, num_particle, dim_state)`.
         """
-        num_obs_time, dim_observation = observation_sequence.shape
         observation_time_indices = np.sort(observation_time_indices)
         num_observation_time = len(observation_time_indices)
         assert observation_sequence.shape[0] == num_observation_time
@@ -109,29 +111,36 @@ class AbstractEnsembleFilter(abc.ABC):
         results = {}
         if return_particles:
             results["state_particles_sequence"] = np.full(
-                (num_obs_time, num_particle, model.dim_state), np.nan
+                (num_observation_time, num_particle, model.dim_state), np.nan
             )
         self._perform_model_specific_initialization(model, num_particle)
+        observation_index = 0
+        states = None
         with ProgressBar(range(num_step + 1), "Filtering", unit="time-steps") as pb:
-            for s in pb:
-                if s == 0:
-                    state_particles = model.sample_initial_state(rng, num_particle)
-                    t = 0
+            for time_index in pb:
+                previous_states = states
+                if time_index == 0:
+                    predicted_states = model.sample_initial_state(rng, num_particle)
                 else:
-                    state_particles = model.sample_state_transition(
-                        rng, state_particles, s
+                    predicted_states = model.sample_state_transition(
+                        rng, previous_states, time_index - 1
                     )
-                if s == observation_time_indices[t]:
-                    state_particles, statistics = self._assimilation_update(
-                        model, rng, state_particles, observation_sequence[t], s
+                if time_index == observation_time_indices[observation_index]:
+                    states, statistics = self._assimilation_update(
+                        model,
+                        rng,
+                        previous_states,
+                        predicted_states,
+                        observation_sequence[observation_index],
+                        time_index,
                     )
                     for key, statistic_array in statistics.items():
-                        if t == 0:
+                        if observation_index == 0:
                             results[key + "_sequence"] = np.full(
                                 (num_observation_time,) + statistic_array.shape, np.nan
                             )
-                        results[key + "_sequence"][t] = statistic_array
+                        results[key + "_sequence"][observation_index] = statistic_array
                     if return_particles:
-                        results["state_particles_sequence"][t] = state_particles
-                    t += 1
+                        results["state_particles_sequence"][observation_index] = states
+                    observation_index += 1
         return results

dapy/filters/ensemble_kalman.py
@@ -1,10 +1,10 @@
 """Ensemble Kalman filters for inference in state space models."""
 
-from typing import Tuple
+from typing import Tuple, Dict, Optional
 import numpy as np
+from numpy.typing import ArrayLike
 from numpy.random import Generator
 import numpy.linalg as nla
-import scipy.linalg as sla
 from dapy.models.base import AbstractGaussianObservationModel, AbstractModel
 from dapy.filters.base import AbstractEnsembleFilter
 
@@ -36,26 +36,27 @@ class EnsembleKalmanFilter(AbstractEnsembleFilter):
         self,
         model: AbstractModel,
         rng: Generator,
-        state_particles: np.ndarray,
-        observation: np.ndarray,
+        previous_states: Optional[ArrayLike],
+        predicted_states: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        observation_particles = model.sample_observation_given_state(
-            rng, state_particles, time_index
+    ) -> Tuple[ArrayLike, Dict[str, ArrayLike]]:
+        simulated_observations = model.sample_observation_given_state(
+            rng, predicted_states, time_index
         )
-        state_deviations = state_particles - state_particles.mean(0)
-        observation_deviations = observation_particles - observation_particles.mean(0)
-        observation_errors = observation - observation_particles
-        state_particles = (
-            state_particles
+        state_deviations = predicted_states - predicted_states.mean(0)
+        observation_deviations = simulated_observations - simulated_observations.mean(0)
+        observation_errors = observation - simulated_observations
+        states = (
+            predicted_states
             + nla.lstsq(observation_deviations.T, observation_errors.T, rcond=None)[0].T
             @ state_deviations
         )
         return (
-            state_particles,
+            states,
             {
-                "state_mean": state_particles.mean(0),
-                "state_std": state_particles.std(0),
+                "state_mean": states.mean(0),
+                "state_std": states.std(0),
             },
         )
 
@@ -86,19 +87,20 @@ class EnsembleTransformKalmanFilter(AbstractEnsembleFilter):
         self,
         model: AbstractGaussianObservationModel,
         rng: Generator,
-        state_particles: np.ndarray,
-        observation: np.ndarray,
+        previous_states: Optional[ArrayLike],
+        predicted_states: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        num_particle = state_particles.shape[0]
-        state_mean = state_particles.mean(0)
-        state_deviations = state_particles - state_mean
+    ) -> Tuple[ArrayLike, Dict[str, ArrayLike]]:
+        num_particle = predicted_states.shape[0]
+        state_mean = predicted_states.mean(0)
+        state_deviations = predicted_states - state_mean
         # Note: compared to the `observation_particles` variable defined in the
         # perturbed observations EnKF implementation here these observation 'particles'
         # are pre addition of observation noise
-        observation_particles = model.observation_mean(state_particles, time_index)
-        observation_mean = observation_particles.mean(0)
-        observation_deviations = observation_particles - observation_mean
+        simulated_observations = model.observation_mean(predicted_states, time_index)
+        observation_mean = simulated_observations.mean(0)
+        observation_deviations = simulated_observations - observation_mean
         observation_error = observation - observation_mean
         # Let X = state_deviations, Y = observation_deviations, N = num_particle,
         # R = observation_noise_covar, Z = post_state_deviations, I = identity(N)
@@ -117,7 +119,7 @@ class EnsembleTransformKalmanFilter(AbstractEnsembleFilter):
             )
             / (num_particle - 1) ** 0.5,
         )
-        squared_transform_matrix_eigenvalues = 1 / (1 + non_zero_singular_values ** 2)
+        squared_transform_matrix_eigenvalues = 1 / (1 + non_zero_singular_values**2)
         if model.dim_observation < num_particle:
             squared_transform_matrix_eigenvalues = np.concatenate(
                 [
@@ -126,7 +128,7 @@ class EnsembleTransformKalmanFilter(AbstractEnsembleFilter):
                 ]
             )
         transform_matrix = (
-            transform_matrix_eigenvectors * squared_transform_matrix_eigenvalues ** 0.5
+            transform_matrix_eigenvectors * squared_transform_matrix_eigenvalues**0.5
         ) @ transform_matrix_eigenvectors.T
         # Let e = observation_error, x = state_mean, z = post_state_mean and
         # X, Y, R, N, I as above
@@ -158,6 +160,6 @@ class EnsembleTransformKalmanFilter(AbstractEnsembleFilter):
             post_state_mean + post_state_deviations,
             {
                 "state_mean": post_state_mean,
-                "state_std": (post_state_deviations ** 2).mean(0) ** 0.5,
+                "state_std": (post_state_deviations**2).mean(0) ** 0.5,
             },
         )

dapy/filters/kalman.py
@@ -3,6 +3,7 @@
 import abc
 from typing import Optional, Sequence, Tuple, Dict
 import numpy as np
+from numpy.typing import ArrayLike
 from numpy.random import Generator
 import numpy.linalg as nla
 import scipy.linalg as sla
@@ -34,12 +35,12 @@ class AbstractKalmanFilter(abc.ABC):
     def filter(
         self,
         model: AbstractLinearGaussianModel,
-        observation_sequence: np.ndarray,
+        observation_sequence: ArrayLike,
         observation_time_indices: Sequence[int],
         num_sample: int = 0,
         rng: Optional[Generator] = None,
         return_covar: bool = False,
-    ) -> Dict[str, np.ndarray]:
+    ) -> Dict[str, ArrayLike]:
         """Compute means and covariances of Gaussian filtering distributions.
 
         Args:
@@ -74,7 +75,6 @@ class AbstractKalmanFilter(abc.ABC):
                     at all observation time indices. Only returned if `num_sample > 0`.
                     Shape `(num_observation_time, num_sample, dim_state)`.
         """
-        num_obs_time, dim_observation = observation_sequence.shape
         observation_time_indices = np.sort(observation_time_indices)
         num_observation_time = len(observation_time_indices)
         assert observation_sequence.shape[0] == num_observation_time
@@ -136,10 +136,10 @@ class AbstractKalmanFilter(abc.ABC):
     def _prediction_update(
         self,
         model: AbstractLinearGaussianModel,
-        prev_state_mean: np.ndarray,
-        prev_state_covar: np.ndarray,
+        prev_state_mean: ArrayLike,
+        prev_state_covar: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray]:
+    ) -> Tuple[ArrayLike, ArrayLike]:
         """Predict state mean and covariance from values at previous time index.
 
         The prediction update only account for the free model dynamics without any
@@ -159,11 +159,11 @@ class AbstractKalmanFilter(abc.ABC):
     def _assimilation_update(
         self,
         model: AbstractLinearGaussianModel,
-        state_mean: np.ndarray,
-        state_covar: np.ndarray,
-        observation: np.ndarray,
+        state_mean: ArrayLike,
+        state_covar: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray]:
+    ) -> Tuple[ArrayLike]:
         """Adjust state mean and covariance for observations at current time index.
 
         The assimilation update adjust the state mean and covariance from their prior
@@ -220,7 +220,8 @@ class MatrixKalmanFilter(AbstractKalmanFilter):
     """
 
     def __init__(
-        self, use_joseph_form: bool = True,
+        self,
+        use_joseph_form: bool = True,
     ):
         """
         Args:
@@ -232,10 +233,10 @@ class MatrixKalmanFilter(AbstractKalmanFilter):
     def _prediction_update(
         self,
         model: AbstractLinearGaussianModel,
-        state_mean: np.ndarray,
-        state_covar: np.ndarray,
+        state_mean: ArrayLike,
+        state_covar: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray]:
+    ) -> Tuple[ArrayLike, ArrayLike]:
         state_mean = model.state_transition_matrix @ state_mean
         state_covar = model.increment_by_state_noise_covar(
             model.state_transition_matrix
@@ -247,11 +248,11 @@ class MatrixKalmanFilter(AbstractKalmanFilter):
     def _assimilation_update(
         self,
         model: AbstractLinearGaussianModel,
-        state_mean: np.ndarray,
-        state_covar: np.ndarray,
-        observation: np.ndarray,
+        state_mean: ArrayLike,
+        state_covar: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray]:
+    ) -> Tuple[ArrayLike, ArrayLike]:
         observation_mean = model.observation_matrix @ state_mean
         observation_covar = model.increment_by_observation_noise_covar(
             model.observation_matrix @ state_covar @ model.observation_matrix.T
@@ -287,14 +288,15 @@ class FunctionKalmanFilter(AbstractKalmanFilter):
     def _prediction_update(
         self,
         model: AbstractLinearGaussianModel,
-        state_mean: np.ndarray,
-        state_covar: np.ndarray,
+        state_mean: ArrayLike,
+        state_covar: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray]:
+    ) -> Tuple[ArrayLike, ArrayLike]:
         state_mean = model.next_state_mean(state_mean, time_index)
         state_covar = model.increment_by_state_noise_covar(
             model.next_state_mean(
-                model.next_state_mean(state_covar, time_index).T, time_index,
+                model.next_state_mean(state_covar, time_index).T,
+                time_index,
             ),
         )
         return state_mean, state_covar
@@ -302,11 +304,11 @@ class FunctionKalmanFilter(AbstractKalmanFilter):
     def _assimilation_update(
         self,
         model: AbstractLinearGaussianModel,
-        state_mean: np.ndarray,
-        state_covar: np.ndarray,
-        observation: np.ndarray,
+        state_mean: ArrayLike,
+        state_covar: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray]:
+    ) -> Tuple[ArrayLike, ArrayLike]:
         observation_mean = model.observation_mean(state_mean, time_index)
         observation_covar = model.increment_by_observation_noise_covar(
             model.observation_mean(

dapy/filters/local.py
@@ -6,6 +6,7 @@ from typing import Tuple, Dict, Callable, Any, Optional, Sequence
 from functools import partial
 import numpy as np
 import numpy.linalg as nla
+from numpy.typing import ArrayLike
 from numpy.random import Generator
 from scipy.special import logsumexp
 from dapy.filters.base import AbstractEnsembleFilter
@@ -35,7 +36,7 @@ class AbstractLocalEnsembleFilter(AbstractEnsembleFilter):
         self,
         localisation_radius: float,
         localisation_weighting_func: Callable[
-            [np.ndarray, float], np.ndarray
+            [ArrayLike, float], ArrayLike
         ] = gaspari_and_cohn_weighting,
         inflation_factor: float = 1.0,
     ):
@@ -63,13 +64,15 @@ class AbstractLocalEnsembleFilter(AbstractEnsembleFilter):
         self.inflation_factor = inflation_factor
 
     def _perform_model_specific_initialization(
-        self, model: AbstractDiagonalGaussianObservationModel, num_particle: int,
+        self,
+        model: AbstractDiagonalGaussianObservationModel,
+        num_particle: int,
     ):
         self._observation_indices_and_weights_cache = [None] * model.mesh_size
 
     def _observation_indices_and_weights(
         self, node_index: int, model: AbstractDiagonalGaussianObservationModel
-    ) -> Tuple[Sequence[int], np.ndarray]:
+    ) -> Tuple[Sequence[int], ArrayLike]:
         if self._observation_indices_and_weights_cache[node_index] is not None:
             return self._observation_indices_and_weights_cache[node_index]
         observation_distances = model.distances_from_mesh_node_to_observation_points(
@@ -91,34 +94,29 @@ class AbstractLocalEnsembleFilter(AbstractEnsembleFilter):
         self,
         model: AbstractDiagonalGaussianObservationModel,
         rng: Generator,
-        state_particles: np.ndarray,
-        observation: np.ndarray,
+        previous_states: Optional[ArrayLike],
+        predicted_states: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        num_particle = state_particles.shape[0]
-        state_particles_mesh = state_particles.reshape(
+    ) -> Tuple[ArrayLike, Dict[str, ArrayLike]]:
+        num_particle = predicted_states.shape[0]
+        predicted_states_mesh = predicted_states.reshape(
             (num_particle, -1, model.mesh_size)
         )
-        observation_means = model.observation_mean(state_particles, time_index)
-        post_state_particles_mesh = np.full(state_particles_mesh.shape, np.nan)
+        observation_means = model.observation_mean(predicted_states, time_index)
+        post_states_mesh = np.full(predicted_states_mesh.shape, np.nan)
         for node_index in range(model.mesh_size):
             local_indices, local_weights = self._observation_indices_and_weights(
                 node_index, model
             )
-            node_state_particles = state_particles_mesh[:, :, node_index]
-            local_observation_means = observation_means[:, local_indices]
-            local_observation = observation[local_indices]
-            local_observation_noise_std = model.observation_noise_std[local_indices]
-            post_state_particles_mesh[
-                :, :, node_index
-            ] = self._local_assimilation_update(
-                node_state_particles,
-                local_observation_means,
-                local_observation,
-                local_observation_noise_std,
+            post_states_mesh[:, :, node_index] = self._local_assimilation_update(
+                predicted_states_mesh[:, :, node_index],
+                observation_means[:, local_indices],
+                observation[local_indices],
+                model.observation_noise_std[local_indices],
                 local_weights,
             )
-        post_state_particles = post_state_particles_mesh.reshape((num_particle, -1))
+        post_state_particles = post_states_mesh.reshape((num_particle, -1))
         return (
             post_state_particles,
             {
@@ -130,12 +128,12 @@ class AbstractLocalEnsembleFilter(AbstractEnsembleFilter):
     @abc.abstractmethod
     def _local_assimilation_update(
         self,
-        node_state_particles: np.ndarray,
-        local_observation_particles: np.ndarray,
-        local_observation: np.ndarray,
-        local_observation_noise_std: np.ndarray,
-        local_observation_weights: np.ndarray,
-    ) -> np.ndarray:
+        node_state_particles: ArrayLike,
+        local_observation_particles: ArrayLike,
+        local_observation: ArrayLike,
+        local_observation_noise_std: ArrayLike,
+        local_observation_weights: ArrayLike,
+    ) -> ArrayLike:
         """Perform a local analysis update for the state at a grid point.
 
         Args:
@@ -191,15 +189,15 @@ class LocalEnsembleTransformParticleFilter(AbstractLocalEnsembleFilter):
         self,
         localisation_radius: float,
         localisation_weighting_func: Callable[
-            [np.ndarray, float], np.ndarray
+            [ArrayLike, float], ArrayLike
         ] = gaspari_and_cohn_weighting,
         inflation_factor: float = 1.0,
         optimal_transport_solver: Callable[
-            [np.ndarray, np.ndarray, np.ndarray], np.ndarray
+            [ArrayLike, ArrayLike, ArrayLike], ArrayLike
         ] = optimal_transport.solve_optimal_transport_exact,
         optimal_transport_solver_kwargs: Optional[Dict[str, Any]] = None,
         transport_cost: Callable[
-            [np.ndarray, np.ndarray], np.ndarray
+            [ArrayLike, ArrayLike], ArrayLike
         ] = optimal_transport.pairwise_euclidean_distance,
         weight_threshold: float = 1e-8,
     ):
@@ -262,17 +260,17 @@ class LocalEnsembleTransformParticleFilter(AbstractLocalEnsembleFilter):
 
     def _local_assimilation_update(
         self,
-        node_state_particles: np.ndarray,
-        local_observation_particles: np.ndarray,
-        local_observation: np.ndarray,
-        local_observation_noise_std: np.ndarray,
-        local_observation_weights: np.ndarray,
-    ) -> np.ndarray:
+        node_state_particles: ArrayLike,
+        local_observation_particles: ArrayLike,
+        local_observation: ArrayLike,
+        local_observation_noise_std: ArrayLike,
+        local_observation_weights: ArrayLike,
+    ) -> ArrayLike:
         num_particle = node_state_particles.shape[0]
         local_observation_errors = local_observation_particles - local_observation
         node_log_particle_weights = -0.5 * (
             local_observation_errors
-            * (local_observation_weights / local_observation_noise_std ** 2)
+            * (local_observation_weights / local_observation_noise_std**2)
             * local_observation_errors
         ).sum(-1)
         node_source_dist = np.ones(num_particle) / num_particle
@@ -312,12 +310,12 @@ class LocalEnsembleTransformKalmanFilter(AbstractLocalEnsembleFilter):
 
     def _local_assimilation_update(
         self,
-        node_state_particles: np.ndarray,
-        local_observation_particles: np.ndarray,
-        local_observation: np.ndarray,
-        local_observation_noise_std: np.ndarray,
-        local_observation_weights: np.ndarray,
-    ) -> np.ndarray:
+        node_state_particles: ArrayLike,
+        local_observation_particles: ArrayLike,
+        local_observation: ArrayLike,
+        local_observation_noise_std: ArrayLike,
+        local_observation_weights: ArrayLike,
+    ) -> ArrayLike:
         num_particle = node_state_particles.shape[0]
         dim_observation_local = local_observation.shape[0]
         # Compute local state ensemble mean vector and deviations matrix
@@ -332,14 +330,14 @@ class LocalEnsembleTransformKalmanFilter(AbstractLocalEnsembleFilter):
         # Compute reciprocal of effective per observation variances
         # by scaling by the inverse variances by the localisation weights
         effective_inv_observation_variance = (
-            local_observation_weights / local_observation_noise_std ** 2
+            local_observation_weights / local_observation_noise_std**2
         )
         transform_matrix_eigenvectors, non_zero_singular_values, _ = nla.svd(
             local_observation_deviations
-            * effective_inv_observation_variance ** 0.5
+            * effective_inv_observation_variance**0.5
             / (num_particle - 1) ** 0.5,
         )
-        squared_transform_matrix_eigenvalues = 1 / (1 + non_zero_singular_values ** 2)
+        squared_transform_matrix_eigenvalues = 1 / (1 + non_zero_singular_values**2)
         if dim_observation_local < num_particle:
             squared_transform_matrix_eigenvalues = np.concatenate(
                 [
@@ -348,7 +346,7 @@ class LocalEnsembleTransformKalmanFilter(AbstractLocalEnsembleFilter):
                 ]
             )
         transform_matrix = (
-            transform_matrix_eigenvectors * squared_transform_matrix_eigenvalues ** 0.5
+            transform_matrix_eigenvectors * squared_transform_matrix_eigenvalues**0.5
         ) @ transform_matrix_eigenvectors.T
         kalman_gain_mult_observation_error = node_state_deviations.T @ (
             transform_matrix_eigenvectors
@@ -383,14 +381,12 @@ class ScalableLocalEnsembleTransformParticleFilter(AbstractEnsembleFilter):
         self,
         localisation_radius: float,
         partition_of_unity: Optional[AbstractPartitionOfUnity] = None,
-        calculate_cost_matrices_func: Optional[
-            Callable[[np.ndarray], np.ndarray]
-        ] = None,
+        calculate_cost_matrices_func: Optional[Callable[[ArrayLike], ArrayLike]] = None,
         localisation_weighting_func: Callable[
-            [np.ndarray, float], np.ndarray
+            [ArrayLike, float], ArrayLike
         ] = gaspari_and_cohn_weighting,
         optimal_transport_solver: Callable[
-            [np.ndarray, np.ndarray, np.ndarray], np.ndarray
+            [ArrayLike, ArrayLike, ArrayLike], ArrayLike
         ] = optimal_transport.solve_optimal_transport_exact_batch,
         optimal_transport_solver_kwargs: Optional[Dict[str, Any]] = None,
         calculate_cost_matrices_func_kwargs: Optional[Dict[str, Any]] = None,
@@ -458,7 +454,9 @@ class ScalableLocalEnsembleTransformParticleFilter(AbstractEnsembleFilter):
         )
 
     def _perform_model_specific_initialization(
-        self, model: AbstractDiagonalGaussianObservationModel, num_particle: int,
+        self,
+        model: AbstractDiagonalGaussianObservationModel,
+        num_particle: int,
     ):
         if self.partition_of_unity is None:
             self.partition_of_unity = PerMeshNodePartitionOfUnityBasis(model)
@@ -496,15 +494,16 @@ class ScalableLocalEnsembleTransformParticleFilter(AbstractEnsembleFilter):
         self,
         model: AbstractDiagonalGaussianObservationModel,
         rng: Generator,
-        state_particles: np.ndarray,
-        observation: np.ndarray,
+        previous_states: Optional[ArrayLike],
+        predicted_states: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        num_particle = state_particles.shape[0]
+    ) -> Tuple[ArrayLike, Dict[str, ArrayLike]]:
+        num_particle = predicted_states.shape[0]
         observation_log_densities = (
             -0.5
-            * (model.observation_mean(state_particles, time_index) - observation) ** 2
-            / (model.observation_noise_std ** 2)
+            * (model.observation_mean(predicted_states, time_index) - observation) ** 2
+            / (model.observation_noise_std**2)
         )
         per_patch_log_target_dists = (
             self._per_patch_localisation_weights @ observation_log_densities.T
@@ -514,11 +513,11 @@ class ScalableLocalEnsembleTransformParticleFilter(AbstractEnsembleFilter):
             - logsumexp(per_patch_log_target_dists, axis=-1)[:, None]
         )
         per_patch_source_dists = np.ones_like(per_patch_target_dists) / num_particle
-        state_particles_mesh = state_particles.reshape(
+        predicted_states_mesh = predicted_states.reshape(
             (num_particle, -1, model.mesh_size)
         )
         per_patch_cost_matrices = self.calculate_cost_matrices_func(
-            state_particles_mesh, **self.calculate_cost_matrices_func_kwargs
+            predicted_states_mesh, **self.calculate_cost_matrices_func_kwargs
         )
         if self.weight_threshold > 0:
             per_patch_target_dists[per_patch_target_dists < self.weight_threshold] = 0
@@ -532,19 +531,19 @@ class ScalableLocalEnsembleTransformParticleFilter(AbstractEnsembleFilter):
             )
             * num_particle
         )
-        post_state_particle_patches = np.einsum(
+        post_states_patches = np.einsum(
             "kij,jlkm->ilkm",
             per_patch_transform_matrices,
-            self.partition_of_unity.split_into_patches_and_scale(state_particles_mesh),
+            self.partition_of_unity.split_into_patches_and_scale(predicted_states_mesh),
         )
-        post_state_particles = self.partition_of_unity.combine_patches(
-            post_state_particle_patches
+        post_states = self.partition_of_unity.combine_patches(
+            post_states_patches
         ).reshape((num_particle, model.dim_state))
         return (
-            post_state_particles,
+            post_states,
             {
-                "state_mean": post_state_particles.mean(0),
-                "state_std": post_state_particles.std(0),
-                "per_patch_estimated_ess": 1 / (per_patch_target_dists ** 2).sum(-1),
+                "state_mean": post_states.mean(0),
+                "state_std": post_states.std(0),
+                "per_patch_estimated_ess": 1 / (per_patch_target_dists**2).sum(-1),
             },
         )

dapy/filters/particle.py
@@ -1,8 +1,8 @@
 """Particle filters for inference in state space models."""
 
-import abc
 from typing import Tuple, Dict, Callable, Any, Optional
 import numpy as np
+from numpy.typing import ArrayLike
 from numpy.random import Generator
 from scipy.linalg import cho_factor, cho_solve
 from scipy.special import logsumexp
@@ -12,57 +12,7 @@ from dapy.models.base import AbstractModel, AbstractConditionallyGaussianModel
 import dapy.ot as optimal_transport
 
 
-class AbstractParticleFilter(AbstractEnsembleFilter):
-    """Abstract base class for particle filters."""
-
-    def _calculate_weights(
-        self,
-        model: AbstractModel,
-        states: np.ndarray,
-        observation: np.ndarray,
-        time_index: int,
-    ) -> np.ndarray:
-        """Calculate importance weights for particles given observations."""
-        log_weights = model.log_density_observation_given_state(
-            observation, states, time_index
-        )
-        log_sum_weights = logsumexp(log_weights)
-        return np.exp(log_weights - log_sum_weights)
-
-    @abc.abstractmethod
-    def _assimilation_transform(
-        self, rng: Generator, state_particles: np.ndarray, weights: np.ndarray
-    ) -> np.ndarray:
-        pass
-
-    def _assimilation_update(
-        self,
-        model: AbstractModel,
-        rng: Generator,
-        state_particles: np.ndarray,
-        observation: np.ndarray,
-        time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        weights = self._calculate_weights(
-            model, state_particles, observation, time_index
-        )
-        state_mean = (weights[:, None] * state_particles).sum(0)
-        state_std = (
-            np.sum(weights[:, None] * (state_particles - state_mean) ** 2, axis=0)
-            ** 0.5
-        )
-        state_particles = self._assimilation_transform(rng, state_particles, weights)
-        return (
-            state_particles,
-            {
-                "state_mean": state_mean,
-                "state_std": state_std,
-                "estimated_ess": 1 / (weights ** 2).sum(),
-            },
-        )
-
-
-class BootstrapParticleFilter(AbstractParticleFilter):
+class BootstrapParticleFilter(AbstractEnsembleFilter):
     """Bootstrap particle filter (sequential importance resampling).
 
     The filtering distribution at each observation time index is approximated by
@@ -81,14 +31,55 @@ class BootstrapParticleFilter(AbstractParticleFilter):
            Solution. Markov Processes and Related Fields. 2 (4): 555--580.
     """
 
+    def _calculate_weights(
+        self,
+        model: AbstractModel,
+        states: ArrayLike,
+        observation: ArrayLike,
+        time_index: int,
+    ) -> ArrayLike:
+        """Calculate importance weights for particles given observations."""
+        log_weights = model.log_density_observation_given_state(
+            observation, states, time_index
+        )
+        log_sum_weights = logsumexp(log_weights)
+        return np.exp(log_weights - log_sum_weights)
+
     def _assimilation_transform(self, rng, state_particles, weights):
         """Perform multinomial particle resampling given computed weights."""
         num_particle = state_particles.shape[0]
         resampled_indices = rng.choice(num_particle, num_particle, True, weights)
         return state_particles[resampled_indices]
 
+    def _assimilation_update(
+        self,
+        model: AbstractModel,
+        rng: Generator,
+        previous_states: Optional[ArrayLike],
+        predicted_states: ArrayLike,
+        observation: ArrayLike,
+        time_index: int,
+    ) -> Tuple[ArrayLike, ArrayLike, ArrayLike]:
+        weights = self._calculate_weights(
+            model, predicted_states, observation, time_index
+        )
+        state_mean = (weights[:, None] * predicted_states).sum(0)
+        state_std = (
+            np.sum(weights[:, None] * (predicted_states - state_mean) ** 2, axis=0)
+            ** 0.5
+        )
+        states = self._assimilation_transform(rng, predicted_states, weights)
+        return (
+            states,
+            {
+                "state_mean": state_mean,
+                "state_std": state_std,
+                "estimated_ess": 1 / (weights**2).sum(),
+            },
+        )
 
-class EnsembleTransformParticleFilter(AbstractParticleFilter):
+
+class EnsembleTransformParticleFilter(BootstrapParticleFilter):
     """Ensemble transform particle filter.
 
     The filtering distribution at each observation time index is approximated by
@@ -109,11 +100,11 @@ class EnsembleTransformParticleFilter(AbstractParticleFilter):
     def __init__(
         self,
         optimal_transport_solver: Callable[
-            [np.ndarray, np.ndarray, np.ndarray], np.ndarray
+            [ArrayLike, ArrayLike, ArrayLike], ArrayLike
         ] = optimal_transport.solve_optimal_transport_exact,
         optimal_transport_solver_kwargs: Optional[Dict[str, Any]] = None,
         transport_cost: Callable[
-            [np.ndarray, np.ndarray], np.ndarray
+            [ArrayLike, ArrayLike], ArrayLike
         ] = optimal_transport.pairwise_euclidean_distance,
         weight_threshold: float = 1e-8,
         use_sparse_matrix_multiply: bool = False,
@@ -229,7 +220,9 @@ class OptimalProposalParticleFilter(AbstractEnsembleFilter):
         self._assume_time_homogeneous_model = assume_time_homogeneous_model
 
     def _perform_model_specific_initialization(
-        self, model: AbstractConditionallyGaussianModel, num_particle: int,
+        self,
+        model: AbstractConditionallyGaussianModel,
+        num_particle: int,
     ):
         if self._assume_time_homogeneous_model:
             cov_observations_given_states = model.increment_by_observation_noise_covar(
@@ -248,10 +241,11 @@ class OptimalProposalParticleFilter(AbstractEnsembleFilter):
         self,
         model: AbstractConditionallyGaussianModel,
         rng: Generator,
-        state_particles: np.ndarray,
-        observation: np.ndarray,
+        previous_states: Optional[ArrayLike],
+        predicted_states: ArrayLike,
+        observation: ArrayLike,
         time_index: int,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    ) -> Tuple[ArrayLike, Dict[str, ArrayLike]]:
         if self._assume_time_homogeneous_model:
             cho_factor_cov_observations_given_states = (
                 self._cho_factor_cov_observations_given_states
@@ -272,44 +266,58 @@ class OptimalProposalParticleFilter(AbstractEnsembleFilter):
             state_noise_covar_observation_matrix_T = model.observation_mean(
                 model.state_noise_covar, time_index
             )
-        simulated_observations = model.sample_observation_given_state(
-            rng, state_particles, time_index
-        )
-        state_particles += (
-            state_noise_covar_observation_matrix_T
-            @ (
-                cho_solve(
-                    cho_factor_cov_observations_given_states,
-                    (observation[None] - simulated_observations).T,
-                )
+        if previous_states is not None:
+            # For optimal proposal weights depend only on particles from previous time
+            # step not particles after assimilation update
+            predicted_observation_means = model.observation_mean(
+                model.next_state_mean(previous_states, time_index - 1), time_index
             )
-        ).T
-        observation_diff = (
-            model.observation_mean(state_particles, time_index) - observation[None]
+            observation_diff = predicted_observation_means - observation[None]
+            log_weights = (
+                -(
+                    observation_diff.T
+                    * cho_solve(
+                        cho_factor_cov_observations_given_states,
+                        observation_diff.T,
+                    )
+                ).sum(0)
+                / 2
+            )
+            weights = np.exp(log_weights - logsumexp(log_weights))
+        else:
+            # previous_states is None when time_index == 0, in which case as
+            # initial state distribution is assumed to be Gaussian, optimal proposal
+            # will produce exact samples from filtering distribution and particles
+            # will all have equal weights
+            weights = np.ones(predicted_states.shape[0]) / predicted_states.shape[0]
+        simulated_observations = model.sample_observation_given_state(
+            rng, predicted_states, time_index
         )
-        log_weights = (
-            -(
-                observation_diff.T
-                * cho_solve(
-                    cho_factor_cov_observations_given_states, observation_diff.T,
+        states = (
+            predicted_states
+            + (
+                state_noise_covar_observation_matrix_T
+                @ (
+                    cho_solve(
+                        cho_factor_cov_observations_given_states,
+                        (observation[None] - simulated_observations).T,
+                    )
                 )
-            ).sum(0)
-            / 2
+            ).T
         )
-        weights = np.exp(log_weights - logsumexp(log_weights))
-        state_mean = (weights[:, None] * state_particles).sum(0)
-        state_std = (
-            np.sum(weights[:, None] * (state_particles - state_mean) ** 2, axis=0)
-            ** 0.5
-        )
-        num_particle = state_particles.shape[0]
-        resampled_indices = rng.choice(num_particle, num_particle, True, weights)
-        state_particles = state_particles[resampled_indices]
+        state_mean = (weights[:, None] * states).sum(0)
+        state_std = np.sum(weights[:, None] * (states - state_mean) ** 2, axis=0) ** 0.5
+        if previous_states is not None:
+            # Skip resampling step if assimilating observations at time_index == 0 as
+            # in this case we have exact samples from filtering distribution
+            num_particle = states.shape[0]
+            resampled_indices = rng.choice(num_particle, num_particle, True, weights)
+            states = states[resampled_indices]
         return (
-            state_particles,
+            states,
             {
                 "state_mean": state_mean,
                 "state_std": state_std,
-                "estimated_ess": 1 / (weights ** 2).sum(),
+                "estimated_ess": 1 / (weights**2).sum(),
             },
         )